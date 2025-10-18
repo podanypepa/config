53868e902aa1b53bf022e41d01c3453a50c4f73f
Josef Podany <podany.pepa@gmail.com>
2025-10-18 10:32:12 +0200

    add neotree plugin

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,6 +8,19 @@ return {
 			"nvim-tree/nvim-web-devicons", -- optional, but recommended
 		},
 		lazy = false, -- neo-tree will lazily load itself
+		config = function()
+			require("neo-tree").setup({
+				popup_border_style = "NC", -- or "" to use 'winborder' on Neovim v0.11+
+				enable_git_status = true,
+				enable_diagnostics = true,
+				open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+				window = {
+					position = "right",
+					width = 40,
+				},
+				default_component_configs = {},
+			})
+		end,
 	},
 	{
 		"antosha417/nvim-lsp-file-operations",
