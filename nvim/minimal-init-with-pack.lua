31d2d38f7df4220fba9f1adf27efd346b748e420
Josef Podany <podany.pepa@gmail.com>
2025-11-13 17:51:05 +0100

    update minimal-init-with-pack

--- nvim/minimal-init-with-pack.lua
+++ nvim/minimal-init-with-pack.lua
@@ -5,7 +5,11 @@ vim.opt.tabstop = 4
 vim.opt.swapfile = false
 vim.opt.signcolumn = "yes"
 vim.opt.winborder = "rounded"
-
+vim.opt.ignorecase = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.undofile = false
+vim.opt.incsearch = true
 vim.g.mapleader = " "
 
 vim.packadd({
@@ -18,9 +22,42 @@ vim.packadd({
 	{
 		src = "https://github.com/neovim/nvim-lspconfig",
 	},
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+	},
+	{
+		src = "https://github.com/mason-org/mason.nvim",
+	},
+	{
+		src = "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+	},
 })
 
+require("mason").setup()
+
 vim.lsp.enable({ "lua_ls", "svelte-language-server" })
+vim.lsp.config.lua_ls.setup({
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+		},
+	},
+})
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "lua", "svelte", "javascript", "typescript", "html", "css" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
 
 vim.colorscheme = "vague"
 -- vim.cmd.colorscheme("vague")
